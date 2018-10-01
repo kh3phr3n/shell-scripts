666770f5522bb4e0bc2da86f7782c95b8993eda9
kh3phr3n <rc.dev@tuxico.com>
2018-10-01 20:09:56 +0200

    Update fonts.sh (file paths...)

--- arch/fonts.sh
+++ arch/fonts.sh
@@ -3,40 +3,45 @@
 # Format: diff:pattern:oldvalue:newvalue:file
 UPDATES=(
     # Xft dpi
-    '1:dpi:166:96:~/.config/xrdb/xft/config'
+    "1:dpi:166:96:$HOME/.config/xrdb/xft/config"
     # Pango font size
-    '0:pango:12:15:~/.config/i3/config'
+    "0:pango:12:15:$HOME/.config/i3/config"
     # Title font size
-    '0:title:12:15:~/.config/i3/config'
+    "0:title:12:15:$HOME/.config/i3/config"
     # Dmenu font size
-    '1:dmenu:-12:-15:~/.config/i3/config'
+    "1:dmenu:-12:-15:$HOME/.config/i3/config"
     # Termite font size
-    '1:font:12:15:~/.config/termite/config'
+    "1:font:12:15:$HOME/.config/termite/config"
     # URxvt font size
-    '1:size:12:15:~/.config/xrdb/urxvt/config'
+    "1:size:12:15:$HOME/.config/xrdb/urxvt/config"
     # Gtk* font size
-    '1:font:12:15:~/.gtkrc-2.0'
-    '1:font:12:15:~/.config/gtk-3.0/settings.ini'
+    "1:font:12:15:$HOME/.gtkrc-2.0"
+    "1:font:12:15:$HOME/.config/gtk-3.0/settings.ini"
     # VSCode zoom level
-    '1:zoom:1.0:2.0:~/.config/Code/User/settings.json'
+    "1:zoom:1.0:2.0:$HOME/.config/Code/User/settings.json"
 )
 
 # Let's update files
 for update in ${UPDATES[@]}
 do
-    local diff=$(echo $update | cut -d: -f1)
-    local patn=$(echo $update | cut -d: -f2)
-    local oldv=$(echo $update | cut -d: -f3)
-    local newv=$(echo $update | cut -d: -f4)
-    local file=$(echo $update | cut -d: -f5)
+    diff=$(echo $update | cut -d: -f1)
+    patn=$(echo $update | cut -d: -f2)
+    oldv=$(echo $update | cut -d: -f3)
+    newv=$(echo $update | cut -d: -f4)
+    file=$(echo $update | cut -d: -f5)
 
+    clear
     # Create backup file
     test ! -e ${file}_ && cp ${file} ${file}_
     # Update current file
     sed -i "/${patn}/s/${oldv}/${newv}/g" ${file}
-    # Inspect current update
-    test ${diff} -ne 0 && git diff ${file}_ ${file}
-    # Clean backup file
-    rm ${file}_ && echo -e "\n:: Press any key to continue..."; read
+
+    if [ ${diff} -ne 0 ]
+    then
+        # Inspect current update
+        git diff ${file}_ ${file}
+        # Clean backup file
+        rm ${file}_ && echo -e "\n:: ..."; read
+    fi
 done
 
