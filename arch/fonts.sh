e098f8986f3a4e03e71de68709411577bcfcdeed
kh3phr3n <rc.dev@tuxico.com>
2018-10-05 14:40:45 +0200

    Update fonts.sh (add parameters)

--- arch/fonts.sh
+++ arch/fonts.sh
@@ -1,24 +1,28 @@
 #!/bin/bash
 
+XD='96'
+SZ='15'
+ZL='2.0'
+
 # Format: diff:pattern:oldvalue:newvalue:file
 UPDATES=(
     # Xft dpi
-    "1:dpi:166:96:$HOME/.config/xrdb/xft/config"
+    "1:dpi:166:$XD:$HOME/.config/xrdb/xft/config"
     # Pango font size
-    "0:pango:12:15:$HOME/.config/i3/config"
+    "0:pango:12:$SZ:$HOME/.config/i3/config"
     # Title font size
-    "0:title:12:15:$HOME/.config/i3/config"
+    "0:title:12:$SZ:$HOME/.config/i3/config"
     # Dmenu font size
-    "1:dmenu:-12:-15:$HOME/.config/i3/config"
+    "1:dmenu:-12:-$SZ:$HOME/.config/i3/config"
     # Termite font size
-    "1:font:12:15:$HOME/.config/termite/config"
+    "1:font:12:$SZ:$HOME/.config/termite/config"
     # URxvt font size
-    "1:size:12:15:$HOME/.config/xrdb/urxvt/config"
+    "1:size:12:$SZ:$HOME/.config/xrdb/urxvt/config"
     # Gtk* font size
-    "1:font:12:15:$HOME/.gtkrc-2.0"
-    "1:font:12:15:$HOME/.config/gtk-3.0/settings.ini"
+    "1:font:12:$SZ:$HOME/.gtkrc-2.0"
+    "1:font:12:$SZ:$HOME/.config/gtk-3.0/settings.ini"
     # VSCode zoom level
-    "1:zoom:1.0:2.0:$HOME/.config/vscode/settings.json"
+    "1:zoom:1.0:$ZL:$HOME/.config/vscode/settings.json"
 )
 
 # Let's update files
