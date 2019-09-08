fcf2b09351ee4ef6d12fdce6489b49aae546f76d
kh3phr3n <kh3phr3n@nixsys.fr>
2019-09-08 20:37:30 +0200

    Update iso.sh: image version

--- arch/iso.sh
+++ arch/iso.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
 SDX='/dev/sdb'
-VER="2019.08.01"
+VER="2019.09.01"
 ISO="archlinux-${VER}-x86_64.iso"
 URL="http://archlinux.polymorf.fr/iso/${VER}"
-SHA="4dae830315df4684b4351348f1a0fc49b5c08aa7"
+SHA="67cf5460beb42230a9c07fd6ebc136cbb1181948"
 
 clear
 # Download the new arch iso
