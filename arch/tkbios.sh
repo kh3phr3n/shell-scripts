2feeae7435656d069df40ed1b542c154aa4e2cc7
kh3phr3n <kh3phr3n@nixsys.fr>
2020-11-11 12:43:51 +0100

    Update tkbios.sh: 1.24 -> 1.25

--- arch/tkbios.sh
+++ arch/tkbios.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-# bios: 1.24
+# bios: 1.25
 # geteltorito: 0.6
 # userpages.uni-koblenz.de/~krienke/ftp/noarch/geteltorito/geteltorito/geteltorito.pl
 # wiki.archlinux.org/index.php/Flashing_BIOS_from_Linux#Bootable_optical_disk_emulation
@@ -8,9 +8,9 @@
 
 SDX='/dev/sdb'
 IMG='l380.img'
-ISO='r0rur17w.iso'
+ISO='r0rur18w.iso'
 URL='https://download.lenovo.com/pccbbs/mobiles'
-SHA='af6ff2cff405b984f22412edaf7c0860790af70278a11b8ca82d4117696f7987'
+SHA='5da3d5db5d36ebfb982617d6e3ea02c0d962fc20c11c939e5713a10a8551cda0'
 
 # Geteltorito is required
 [[ ! -e geteltorito.pl ]] && exit 0 || clear
