0da8eea41aef0ea08b31e204269a12865acfbafa
kh3phr3n <kh3phr3n@nixsys.fr>
2022-01-08 16:28:15 +0100

    Update UI font-sizes

--- arch/fonts.sh
+++ arch/fonts.sh
@@ -5,51 +5,63 @@ QHD=true
 
 if [ "${QHD}" == true ]
 then
-    DPI='109'   # Xft dpi
-    MSF='12'    # Monospace fonts
-    SSF='13'    # Sans serif fonts
-    ULS='0'     # URxvt letter spacing
-    AWP='3'     # Alacritty window padding
-    AOX='0'     # Alacritty font offset x
-    AOY='0'     # Alacritty font offset y
-    VZL='1.2'   # VS Code zoom level
-    VFS='12.85' # VS Code editor font size
+    DPI='109'  # Xft dpi
+    MSF='11'   # Monospace fonts
+    SSF='11'   # Sans serif fonts
+    ULS='0'    # URxvt letter spacing
+    AWP='3'    # Alacritty window padding
+    AOX='0'    # Alacritty font offset x
+    AOY='0'    # Alacritty font offset y
+    VFS='13'   # VS Code font sizes
+    VZL='0.8'  # VS Code zoom level
+    VELH='17'  # VS Code editor line height
+    VELS='0.2' # VS Code editor letter spacing
+    VTLH='1'   # VS Code terminal line height
+    VTLS='1'   # VS Code terminal letter spacing
 else
     DPI='96'
     MSF='11'
-    SSF='12'
+    SSF='11'
     ULS='0'
     AWP='3'
     AOX='0'
     AOY='0'
+    VFS='13'
     VZL='0.8'
-    VFS='12'
+    VELH='17'
+    VELS='0.1'
+    VTLH='1'
+    VTLS='1'
 fi
 
 # Format: diff:pattern:oldvalue:newvalue:file
 UPDATES=(
     # Xft
-    "1:dpi:156:$DPI:$HOME/.config/xrdb/xft/config"
+    "1:dpi:158:$DPI:$HOME/.config/xrdb/xft/config"
 
     # i3wm
-    "0:pango:11:$MSF:$HOME/.config/i3/config"
-    "0:title:11:$MSF:$HOME/.config/i3/config"
-    "1:dmenu:-11:-$MSF:$HOME/.config/i3/config"
+    "0:pango:10:$MSF:$HOME/.config/i3/config"
+    "0:title:10:$MSF:$HOME/.config/i3/config"
+    "1:dmenu:-10:-$MSF:$HOME/.config/i3/config"
 
     # URxvt
-    "0:size:11:$MSF:$HOME/.config/xrdb/urxvt/config"
+    "0:size:10:$MSF:$HOME/.config/xrdb/urxvt/config"
     "1:letter:0:$ULS:$HOME/.config/xrdb/urxvt/config"
 
     # Alacritty
-    "1:size:11:$MSF:$HOME/.config/alacritty/alacritty.yml"
+    "1:size:10:$MSF:$HOME/.config/alacritty/alacritty.yml"
 
     # Gtk*
-    "1:font:11:$SSF:$HOME/.gtkrc-2.0"
-    "1:font:11:$SSF:$HOME/.config/gtk-3.0/settings.ini"
+    "1:font:10:$SSF:$HOME/.gtkrc-2.0"
+    "1:font:10:$SSF:$HOME/.config/gtk-3.0/settings.ini"
 
     # VS Code
-    "0:zoom:0.6:$VZL:$HOME/.config/vscode/settings.json"
-    "1:editor.fontSize:13:$VFS:$HOME/.config/vscode/settings.json"
+    "0:zoom:0:$VZL:$HOME/.config/vscode/settings.json"
+    "0:fontSize:13:$VFS:$HOME/.config/vscode/settings.json"
+    "0:editor.lineHeight:17:$VELH:$HOME/.config/vscode/settings.json"
+    "0:integrated.lineHeight:1:$VTLH:$HOME/.config/vscode/settings.json"
+    "0:editor.letterSpacing:0.5:$VELS:$HOME/.config/vscode/settings.json"
+    "1:integrated.letterSpacing:1:$VTLS:$HOME/.config/vscode/settings.json"
 )
 
 # Let's update files
