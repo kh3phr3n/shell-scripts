a2fe863265985a946c6fb3933205324aaa33efe7
kh3phr3n <kh3phr3n@nixsys.fr>
2023-03-24 09:52:08 +0100

    Update iso.sh: 2023.03.01

--- arch/iso.sh
+++ arch/iso.sh
@@ -1,16 +1,16 @@
 #!/bin/bash
 
 SDX='/dev/sdc'
-VER="2022.09.03"
+VER="2023.03.01"
 ISO="archlinux-${VER}-x86_64.iso"
-SHA="778b6399dff5955ef0666d4962da2a9c291b59cb"
 URL="https://mirrors.gandi.net/archlinux/iso/${VER}"
+SHA="816758ba8fd8a06dff539b9af08eb8100c8bad526ac19ef4486bce99cd3ca18c"
 
 clear
 # Download the new arch iso
 curl --progress-bar --remote-name ${URL}/${ISO}
 # Check the SHA1 sum
-echo "${SHA} ${ISO}" | sha1sum --check -
+echo "${SHA} ${ISO}" | sha256sum --check -
 # Copy the iso to the usb drive
 sudo dd bs=4M if=${ISO} of=${SDX} status=progress && sync
 
